d26de9c896d35d5822e9150ceacef46c687aaee4
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-02-11 17:58:46 -0500

    fix video View

--- src/components/VideoView/VideoView.tsx
+++ src/components/VideoView/VideoView.tsx
@@ -1,30 +1,37 @@
+import Icono from "../../assets/play icon.png";
+
 export function VideoView() {
   return (
-    <div className="row container mx-auto mt-5 py-5">
-      <div className="col-6 text-white">
-        <h1 className="text-bold mb-4">Big ComeBack</h1>
-        <p className="mb-5 ">
-          Nullam porta, eros id aliquam pulvinar, urna ex mattis eros, quis
-          vestibulum urna turpis et risus. Mauris porttitor risus faucibus,
-          auctor arcu a, tincidunt nibh...
-        </p>
+    <div className="container">
+      <div className="row  mx-auto mt-5 py-5">
+        <div className="col-md-12 col-lg-6 order-md-2 order-lg-1 mt-5">
+          <h1 className="text-bold mb-4 text-white">Big ComeBack</h1>
+          <p className="mb-5 text-white">
+            Nullam porta, eros id aliquam pulvinar, urna ex mattis eros, quis
+            vestibulum urna turpis et risus. Mauris porttitor risus faucibus,
+            auctor arcu a, tincidunt nibh...
+          </p>
 
-        <button type="button" className=" btn btn-info p-3 ">
-          WATCH NOW
-        </button>
-        <button type="button" className=" btn btn-outline-light mx-3 p-3 ">
-          +PLAYLIST
-        </button>
+          <button type="button" className=" btn btn-info  p-3 px-5  mb-3">
+            <span className="text-bold  text-light">WATCH NOW</span>
+          </button>
+          <button
+            type="button"
+            className=" btn btn-outline-light mx-3 p-3 px-5 mb-3"
+          >
+            +PLAYLIST
+          </button>
+        </div>
+        <div className="col-md-12 col-lg-6 d-flex justify-content-center align-items-center order-md-1 order-lg-2">
+          <button
+            type="button"
+            className="btn btn-outline-light mx-2 rounded-circle btn-lg p-4 mb-5"
+          >
+            <img src={Icono} alt="icon" className="ms-2 p-1 w-75 h-25" />
+          </button>
+        </div>
+        <div className="my-4"></div>
       </div>
-      <div className="col-6 d-flex justify-content-center align-items-center">
-        <button
-          type="button"
-          className="btn btn-outline-light mx-2 rounded-circle btn-lg m-auto h-25"
-        >
-          <span>&#62;</span>
-        </button>
-      </div>
-      <div className="my-4"></div>
     </div>
   );
 }
