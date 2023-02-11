f3ccc999bacbadae2c5fb50746fa1f0bbacdbb60
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-02-11 18:34:58 -0500

    temporal

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: false ? "/libel_academy/" : "/",
+  base: "/libel_academy_temporal/",
 });
