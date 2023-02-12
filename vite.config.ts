0144d1d8e9b9d5d909eac5329075fb887fffa17c
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-02-11 19:03:01 -0500

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/libel_academy/",
+  base: "/libel_academy_temporal/",
   assetsInclude: ["./src/assets/play icon.png"],
 });
